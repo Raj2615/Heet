e8ddcb795a486d8e3802e5de2759f3d3bfc10edb
Raj2615 <heetmungra15@gmail.com>
2024-10-12 21:56:04 +0530

    Add files via upload

---
+++ pr1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+
+class pr1{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter Age of sam : ");
+		int age=s.nextInt();
+		System.out.println(age+" is the age of sam");
+	}
+
+}
